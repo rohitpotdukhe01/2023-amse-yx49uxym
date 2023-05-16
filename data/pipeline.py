29accf5ccde0b3febbb42fbf6a8dc06a00ff45db
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2023-05-16 21:48:41 +0200

    Prepared the final pipeline file

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,2 +1,32 @@
 import pandas as pd
+import csv
+import sqlite3
 
+#Fetch and load dataset 1
+url1 = "https://offenedaten-koeln.de/sites/default/files/Radverkehr%20f%C3%BCr%20Offene%20Daten%20K%C3%B6ln%202022.csv"
+df1 = pd.read_csv(url1, delimiter=';', encoding='latin-1')
+
+#Fetch and load dataset 2
+url2 = "https://raw.githubusercontent.com/od-ms/radverkehr-zaehlstellen/main/100035541/2019-01.csv"
+df2 = pd.read_csv(url2, delimiter=',', encoding='latin-1')
+
+#Connect and load into SQLite database
+connection = sqlite3.connect('AMSE_database.db')
+
+df1.to_sql("Bicycle_Traffic_Data_Cologne_2022", connection, if_exists='replace', index=False)
+df2.to_sql("Bicycle_Roads_Cologne", connection, if_exists='replace', index=False)
+
+connection.commit()
+connection.close()
+
+
+conn = sqlite3.connect('AMSE_database.db')
+
+query1 = "SELECT * FROM Bicycle_Traffic_Data_Cologne_2022"
+query2 = "SELECT * FROM Bicycle_Roads_Cologne"
+df3 = pd.read_sql_query(query1, conn)
+df4 = pd.read_sql_query(query2, conn)
+conn.close()
+
+print(df3)
+print(df4)
