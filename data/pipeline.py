589a2c4ca73fe9276d7e30932ccdf0665483c616
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2023-05-30 02:10:13 +0200

    Implemented all the data transformation steps in pipeline.py

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import sqlite3
 
@@ -12,6 +13,25 @@ df_2021 = pd.read_csv(url2, encoding="iso-8859-1",  delimiter=';')
 # #Fetch and load dataset 3
 url3 = "https://offenedaten-koeln.de/sites/default/files/Radverkehr%20f%C3%BCr%20Offene%20Daten%20K%C3%B6ln%202022.csv"
 df_2022 = pd.read_csv(url3, encoding="iso-8859-1",  delimiter=';')
+
+#Data Transformation
+
+#Renaming column names
+df_2020.rename(columns= {'Jahr 2020':'Month'}, inplace=True)
+df_2021.rename(columns= {'Unnamed: 0':'Month'}, inplace=True)
+df_2022.rename(columns= {'Unnamed: 0':'Month'}, inplace=True)
+
+#Repalcing column errors
+df_2020.columns = df_2020.columns.str.replace('Ã¼', 'ü')
+df_2020.columns = df_2020.columns.str.replace('Ã', 'ß')
+df_2020.columns = df_2020.columns.str.replace('Ã¤', 'ä')
+
+#Repalcing row errors
+df_2020.replace({'Ã¼': 'ü', 'Ã ': 'ß', 'Ã¤': 'ä'}, regex=True, inplace=True)
+
+#Fixing NaN values
+df_2021.fillna(0, inplace=True)
+df_2020.fillna(0, inplace=True)
 
 #Connect and load into SQLite database
 connection = sqlite3.connect('AMSE_database.db')
