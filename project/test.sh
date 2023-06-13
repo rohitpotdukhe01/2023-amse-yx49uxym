3982208351b152bfb9b27de7f1ee19c332e07fd4
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2023-06-13 17:40:42 +0200

    trying out something so that github action works

--- project/test.sh
+++ project/test.sh
@@ -2,8 +2,8 @@
 
 # execute the pipeline
 echo "Execute the pipeline"
-python /data/pipeline.py
+python ./data/pipeline.py
 
 # test of pipeline
 echo "Test if pipeline works correctly"
-pytest /data/test.py
+pytest ./data/test.py
