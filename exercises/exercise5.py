24b62d97b6f8156b58ffed9afc74735a19091deb
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2023-07-10 20:24:06 +0200

    added comments and some finishing touches

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -12,17 +12,14 @@ column_picker = ["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"]
 columns_dType = {"stop_id" : Integer, "stop_name" : Text, "stop_lat" : Float, "stop_lon" : Float, "zone_id" : Integer}
 
 
-# Download from URL
+# Data processing
 urllib.request.urlretrieve(url, zip_path)
-
-# Extract
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(file_path)
 
-# Import dataset
+# save to df
 stops_file = file_path + file_name
 df = pd.read_csv(stops_file, sep=',', decimal='.', index_col=False, usecols=column_picker, encoding='utf-8')
-print(df.head())
 
 # Only keep stops from zone 2001
 df = df[df["zone_id"] == 2001]
